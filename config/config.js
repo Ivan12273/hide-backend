bd86b4d6173175139db546afc63b86b94e3ae76a
Ivan <ivanaguilar01@live.com.mx>
2021-03-10 16:30:59 -0600

    Cambios en config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //ENVIRONMENT [dev, local, prod test]
-process.env.NODE_ENV = process.env.NODE_ENV || "dev";
-process.env.APP_NAME = 'Rooster Software'
+process.env.NODE_ENV = process.env.NODE_ENV || "local";
+process.env.APP_NAME = 'Rooster Software';
 
 //PORT
 process.env.PORT = 3700;
@@ -12,23 +12,23 @@ process.env.TOKEN_EXP = '48h';
 if (process.env.NODE_ENV === 'local') {
   process.env.DB_URI = `mongodb://localhost:27017/hidedb`;
 } else if (process.env.NODE_ENV === "dev") {
-  process.env.DB_URI = `mongodb+srv://root:12rooster.@hide.mgqeo.mongodb.net/hide?retryWrites=true&w=majority`;
+  process.env.DB_URI = ``;
 } else {
-  process.env.DB_URI = `mongodb+srv://root:12rooster.@hide.mgqeo.mongodb.net/hide?retryWrites=true&w=majority`;
+  process.env.DB_URI = ``;
 }
 
 //Auth seed
 process.env.AUTH_SEED = process.env.AUTH_SEED || 'este-es-el-seed-desarrollo';
 
 // MAIL
-process.env.MAIL_SENDER_USER = 'erofaba@gmail.com'
-process.env.MAIL_SENDER_PASS = 'epucqywrcptafhht'
-process.env.MAIL_FROM = 'Rooster software'
+process.env.MAIL_SENDER_USER = 'ivanalberto.agca@gmail.com';
+process.env.MAIL_SENDER_PASS = 'epucqywrcptafhht';
+process.env.MAIL_FROM = 'Rooster software';
 
 //FRONT-END
 var host = 'localhost' // o localhost
-process.env.FRONT_URL = `http://${host}:4200`
-process.env.BACK_URL = host
+process.env.FRONT_URL = `http://${host}:4200`;
+process.env.BACK_URL = host;
 
 
 
